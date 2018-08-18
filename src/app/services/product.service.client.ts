9b73e03e7913bc82cf73bac6ea94e51527b40d1f
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-18 19:08:18 -0400

    Removed producer-product service

--- src/app/services/product.service.client.ts
+++ src/app/services/product.service.client.ts
@@ -4,12 +4,61 @@ import { NODE } from './const-url';
 
 @Injectable()
 export class ProductServiceClient {
+  URL = NODE + '/';
+
     findAllActiveProducts() {
         return fetch(NODE + '/api/product/active')
             .then(response => response.json());
     }
-    findProductById(productId) {
-        return fetch(NODE + '/api/product/' + productId)
-            .then(response => response.json());
-    }
+
+  createProduct = (product) =>
+    fetch(this.URL + 'api/product/', {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+      .then(response => response.json())
+
+  findProductById = (productId) =>
+    fetch(this.URL + 'api/product/' + productId, {
+      method: 'get',
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(response => response.json());
+
+  updateProduct = (product) =>
+    fetch(this.URL + 'api/product/' + product._id, {
+      method: 'put',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+      .then(response => response.json())
+
+  findAllProducts = () =>
+    fetch(this.URL + 'api/product/', {
+      method: 'get',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(response => response.json())
+
+  findProductByOCPC = (ocpc) =>
+    fetch(this.URL + 'api/product/ocpc/' + ocpc, {
+      method: 'get',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(response => response.json());
 }
